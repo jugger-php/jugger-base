f6f6b4e499b22b43f9cbda3be3c6c6f90c953ae5
irpsv <i.rpsv@live.com>
2017-01-26 00:02:39 +0500

    refact

--- src/Configurator.php
+++ src/Configurator.php
@@ -2,15 +2,13 @@
 
 namespace jugger\base;
 
-use Exception;
-
 abstract class Configurator
 {
-	public static function setValues($object, array $config) {
+	public static function setValues($object, array $config)
+	{
 		if (empty($config)) {
 			return;
 		}
-
 		foreach ($config as $name => $value) {
 			$object->$name = $value;
 		}
